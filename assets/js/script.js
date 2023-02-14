dbd97e7ea72e682b98e9026555226f6cadd4aaab
LanaCreegan <Lanacreegan17@gmail.com>
2023-02-14 15:18:14 +0000

    Add in javascript for modal

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// modal
+
+var modal = document.getElementById("instruction-modal");
+
+var btn = document.getElementById("instruction-btn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
