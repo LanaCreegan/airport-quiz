dbef2273c421ab4663a69dcba75a27e960ad4d60
LanaCreegan <Lanacreegan17@gmail.com>
2023-02-14 15:43:47 +0000

    Add questions for quiz in quiz.js file

---
+++ assets/js/quiz.js
@@ -0,0 +1,39 @@
+let questions = [{
+    question: "What city uses the code ARN",
+    options: ['Stockholm, Abu Dhabi, Brussles, Vienna'],
+    answer: "Stockholm",
+},
+
+{
+    question: "What city uses the code CPH",
+    options: ['Copenhagen, Chicago, Dubai, Cape Town'],
+    answer: "Copenhagen",
+
+},
+
+{
+    question: "What city uses the code SYD",
+    options: ['Sydney, Syracuse, Stuttgart, Oslo'],
+    answer: "Sydney",
+    
+},
+
+{
+    question: "What city uses the code ORY",
+    options: ['Paris, Orlando, New York, Melbourne'],
+    answer: "Paris",
+},
+
+{
+    question: "What city uses the code NAP",
+    options: ['Naples, Shanghai, Dublin, Nashville'],
+    answer: "Naples",
+},
+
+{
+    question: "What city uses the code DUS",
+    options: ['Dusseldorf, Doha, Nice, Madrid'],
+    answer: "Dusseldorf",
+},
+
+];
