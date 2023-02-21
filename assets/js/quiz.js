0a3594b38228ba24b64ecf40c988c98e8dad5eb0
LanaCreegan <Lanacreegan17@gmail.com>
2023-02-21 21:33:55 +0000

    Modal bug fixes and JS comments

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,16 +1,17 @@
 (function(){
-
+    //generate quiz questions
     function generateQuiz(){
-
+      //empty html array
       const html = [];
+      //loop through questions
       Questions.forEach(
         (currentQuestion, questionNumber) => {
-            console.log(currentQuestion);
-            console.log(questionNumber);
 
+            //empty answers array
             const countries = [];
-            for(answerIndex in currentQuestion.answers){
-                console.log(answerIndex);
+            //loop through answers
+            for(let answerIndex in currentQuestion.answers){
+                //add html input to array
                 countries.push(
                     `<label>
                       <input type="radio" name="question${questionNumber}" value="${answerIndex}">
@@ -19,28 +20,42 @@
                     </label>`
                   );
             }
-            console.log(countries);
+            //push question and answers array to question block
             html.push(
                 `<div class="question-block">
                   <div class="question"> <p>${currentQuestion.question}</p></div>
                   <div class="answers"> ${countries.join("")} </div>
                 </div>`
               );
-            console.log(html);
+            //add all questions to the game container
             gameContainer.innerHTML = html.join('');
         }
       );
+      //make the first question visable
       document.querySelectorAll('.question-block')[0].classList.add('show');
     }
+
+    //function to show next question
     function nextQuestion(){
+
+      //get current question
       let question = document.getElementsByClassName('show');
+
+      //check if there is a next question
       if(question[0].nextSibling === null){
-        
+        console.log("No more question")
+      //else make next display block and current display none
       }else{
+        //addd show class to next question
         question[0].nextSibling.classList.add('show');
+        //remove show class from current question
         question[0].classList.remove('show');
+        //increase the question counter
         questionIndicator++;
+        //update front end with question counter
         document.getElementById('question-number').innerHTML = questionIndicator;
+
+        //after making last question show make submit button appear and remove next button
         if(question[0].nextSibling === null){
           document.getElementById('submit').classList.add("show");
           document.getElementById('next').classList.add("hide");
@@ -48,41 +63,56 @@
       }
     }
 
+    //Add up the correct questions
     function generateResults(){
-      console.log("generate results")
-
+      
+      //get all the answers
       const answers = gameContainer.querySelectorAll('.answers');
 
+      //init result counter
       let resultCounter = 0;
 
-      console.log(answers);
+      //loop through all the questions
       Questions.forEach( (currentQuestion, questionNumber) => {
         
-        // find selected answer
+        // get current answers in loop 
         const answerContainer = answers[questionNumber];
+
+        //get the selector for current question in loop
         const selector = `input[name=question${questionNumber}]:checked`;
+
+        //get which answer the user selected
         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-        console.log(answerContainer);
-        console.log(selector);
-        console.log(userAnswer);
-
+        //check if users answer matchs the correct answer in the array of questions
         if(userAnswer === currentQuestion.correctAnswer){
+          //if the answer is correct increment the result counter
           resultCounter ++;
         }
-        console.log(resultCounter);
+        
+        //set the front end indicator for the results to the number of questions the user got out of the amount of questions
         document.getElementById("quiz-result").innerHTML = `You got ${resultCounter}/${Questions.length} correct.`;
+
+        //remove the last question from the screen
         let question = document.getElementsByClassName('show');
         question[0].classList.remove('show');
 
+        //make the back to homepage button appear
         document.getElementById("back").classList.add('show');
       });
     }
 
+    //submit button event listener
     document.getElementById('submit').addEventListener('click', generateResults);
+    //next button event listener
     document.getElementById('next').addEventListener('click', nextQuestion);
+
+    //get the quiz container where the questons will be injected
     const gameContainer = document.getElementById('quiz');
+    //curent question indicator
     let questionIndicator = 1;
+
+    //array of questions and their answers
     const Questions = [
       {
         question: "What city uses the code ARN",
@@ -149,6 +179,7 @@
       }
     ];
 
+    //Generate the quiz on load
     generateQuiz();
   })();
   
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
-
-let modal = document.getElementById("instruction-modal");
-let btn = document.getElementById("modal-btn");
-let span = document.getElementsByClassName("close");
-
-
+// get the modal
+var modal = document.getElementById("myModal");
+//get the open modal button
+var btn = document.getElementById("myBtn");
+//get the close modal button
+var span = document.getElementsByClassName("close")[0];
+ 
+//when the button is clicked set the modals display to block
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-
+//when the close button is clicked set the modals display to none
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-
+//if a user clicks off the modal close the modal
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
